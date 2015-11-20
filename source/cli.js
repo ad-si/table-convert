7024f0a7a3faa3f5327b614b5b20203f558a80ab
Adrian Sieber <mail@adriansieber.com>
2015-11-20 13:41:52 +0100

    Test command line interface

--- source/cli.js
+++ source/cli.js
@@ -10,11 +10,12 @@ let args = process.argv
 let commandName = path.basename(args[1])
 let fileContent = fs.readFileSync(path.resolve(args[2]))
 
+
 if (commandName === 'html-table2mmd')
 	process.stdout.write(tableConvert(fileContent, {format: 'markdown'}))
 
-else if (commandName === 'html-table2latex')
-	process.stdout.write(tableConvert(fileContent)))
+else if (commandName === 'html-table2latex' || commandName === 'cli.js')
+	process.stdout.write(tableConvert(fileContent))
 
 else
 	throw new Error(args[1] + ' is no available command name.')
--- test/index.js
+++ test/index.js
@@ -2,19 +2,14 @@
 
 const fs = require('fs')
 const path = require('path')
+const child_process = require('child_process')
 const expect = require('unexpected')
 const tableConvert = require('../source/index.js')
 
-{
-	let actual = tableConvert(
-		fs.readFileSync(path.join(__dirname, './test.html'))
-	)
-	let expected = fs.readFileSync(path.join(__dirname, './test.tex'), 'utf8')
-
-	expect(actual, 'to be', expected)
-}
 
 {
+	process.stdout.write('It converts HTML to Markdown')
+
 	let actual = tableConvert(
 		fs.readFileSync(path.join(__dirname, './test.html')),
 		{
@@ -24,4 +19,39 @@ const tableConvert = require('../source/index.js')
 	let expected = fs.readFileSync(path.join(__dirname, './table.md'), 'utf8')
 
 	expect(actual, 'to be', expected)
+	console.log(' ✔')
+}
+
+{
+	let expectedLatex = fs.readFileSync(
+		path.join(__dirname, './test.tex'),
+		'utf8'
+	)
+
+	{
+		process.stdout.write('It converts HTML to Latex')
+
+		let actual = tableConvert(
+			fs.readFileSync(path.join(__dirname, './test.html'))
+		)
+		expect(actual, 'to be', expectedLatex)
+		console.log(' ✔')
+	}
+
+	{
+		process.stdout.write('It has a command line interface')
+
+		let actual = child_process.execFileSync(
+			'source/cli.js',
+			[
+				'test/test.html'
+			],
+			{
+				cwd: path.join(__dirname, '../')
+			}
+		)
+
+		expect(actual.toString(), 'to be', expectedLatex)
+		console.log(' ✔')
+	}
 }
